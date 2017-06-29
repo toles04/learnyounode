db743874847f7832e98d12692c6e0784ff94d9fc
Tolu Akinnubi <tolu.akinnubi@yahoo.com>
2017-06-29 10:44:08 +0000

    add HTTP File Server

---
+++ http-file-server.js
@@ -0,0 +1,10 @@
+var http = require('http')
+var fs = require('fs')
+ 
+var port = process.argv[2]
+var file = process.argv[3]
+ 
+http.createServer(function (request, response) {
+   // res.writeHead(200, { 'content-type': 'text/plain' })
+  fs.createReadStream(file).pipe(response)
+}).listen(Number(process.argv[2]))
